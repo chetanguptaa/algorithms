f681aed0c1cc665af9319a44dbefaad760f3709a
chetan gupta <chetangupta123raj@gmail.com>
2023-04-07 15:40:51 +0530

    updated linked.java

--- files/linked.java
+++ files/linked.java
@@ -1,4 +1,5 @@
 public class linked {
+
     private ListNode head;
 
     private static class ListNode {
@@ -9,5 +10,18 @@ public class linked {
             this.data = data;
             this.next = null;
         }
+    }
+    public static void main(String[] args) {
+        linked sll  = new linked();
+        sll.head = new ListNode(10);
+        ListNode second = new ListNode(1);
+        ListNode third = new ListNode(8);
+        ListNode fourth = new ListNode(11);
+
+        // Now we will connect them together to form a chain
+
+        sll.head.next = second;
+        second.next = third;
+        third.next = fourth;
     }
 }
