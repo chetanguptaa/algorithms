8c98505fb233e5dbce736fc3106b5dc4306b72d8
chetan gupta <chetangupta123raj@gmail.com>
2023-04-07 15:35:27 +0530

    added linked.java

---
+++ files/linked.java
@@ -0,0 +1,13 @@
+public class linked {
+    private ListNode head;
+
+    private static class ListNode {
+        private int data; //Generic type
+        private ListNode next;
+
+        public ListNode(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+}
