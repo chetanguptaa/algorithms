3b25dea09cb227f48bbbc38cdee6bbc4395aa574
chetan gupta <chetangupta123raj@gmail.com>
2023-04-09 19:13:35 +0530

    added SelectionSort.java

---
+++ files/sorting/SelectionSort.java
@@ -0,0 +1,33 @@
+package sorting;
+
+public class SelectionSort {
+
+    public void printArray(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+           System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+     
+    public void sort(int[] arr) {
+        for(int i = 0; i < arr.length - 1; i++) {
+            int min = i;
+            for(int j = i + 1; j < arr.length; j++) {
+                if(arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = new int[] { 5, 1, 2, 9, 10 };
+        SelectionSort ss = new SelectionSort();
+        ss.printArray(arr);
+        ss.sort(arr);
+        ss.printArray(arr);
+    }
+}
