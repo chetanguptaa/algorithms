630db7efe212b7ecb350b3946853744b2e9b478d
chetan gupta <chetangupta123raj@gmail.com>
2023-04-09 18:40:43 +0530

    added BubbleSort.java

---
+++ files/sorting/BubbleSort.java
@@ -0,0 +1,37 @@
+package sorting;
+
+public class BubbleSort {
+    public void printArray(int[] arr) {
+        for(int  i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public void sort(int[] arr) {
+        boolean isSwapped;
+
+        for(int i = 0; i < arr.length - 1; i++) {
+            isSwapped = false;
+            for(int j = 0 ; j < arr.length - 1 - i; j++) {
+                if(arr[j] > arr[j+1] ) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    isSwapped = true;
+                }
+            }
+            if(isSwapped == false) {
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[] { 5, 1, 2, 9, 10, 3};
+        BubbleSort bs = new BubbleSort();
+        bs.printArray(arr);
+        bs.sort(arr);
+        bs.printArray(arr);  
+    }
+}
