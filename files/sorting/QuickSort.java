d39771af34c5e5c0f7fe7579f68babb0f2d40938
chetan gupta <chetangupta123raj@gmail.com>
2023-04-10 14:41:07 +0530

    added QuickSort.java

---
+++ files/sorting/QuickSort.java
@@ -0,0 +1,19 @@
+package sorting;
+
+public class QuickSort {
+
+    public void printArray(int[] arr) {
+        for(int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void sort(int[] arr, int left, int right) {
+        
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
