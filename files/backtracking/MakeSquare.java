6a305de4483df0bff61387ef674c37a37373ddc0
chetanguptaa <chetangupta123raj@gmail.com>
2023-11-08 13:21:29 +0530

    update: files

--- files/backtracking/MakeSquare.java
+++ files/backtracking/MakeSquare.java
@@ -1,5 +1,7 @@
 package backtracking;
 
+import java.util.Arrays;
+
 public class MakeSquare {
     public static void main(String[] args) {
         System.out.println(makesquare(new int[] {1,1,2,2,2}));
@@ -9,6 +11,8 @@ public class MakeSquare {
         int sum = 0;
         for(int matchstick: matchsticks) sum += matchstick;
         if(sum % 4 != 0) return false;
+        Arrays.sort(matchsticks);
+        reverse(matchsticks);
         return dfs(matchsticks, new int[4], 0, sum / 4);
     }
     private static boolean dfs(int[] matchsticks, int[] sums, int index, int target) {
@@ -23,5 +27,15 @@ public class MakeSquare {
             sums[i] -= matchsticks[index];
         }
         return false;
+    }
+    private static void reverse(int[] nums) {
+        int i = 0, j = nums.length - 1;
+        while (i < j) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+            j--;
+        }
     }
 }
