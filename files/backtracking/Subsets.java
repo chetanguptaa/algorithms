d98884f8db98af4ab88957d44fda1b3587b69de1
chetan gupta <chetangupta123raj@gmail.com>
2023-11-20 15:05:27 +0530

    update dp

--- files/backtracking/Subsets.java
+++ files/backtracking/Subsets.java
@@ -5,13 +5,15 @@ import java.util.*;
 public class Subsets {
     public static void main(String[] args) {
         // System.out.println(subsets(new int[]{3, 2, 1}));
-        System.out.println(subsets2(new int[] {1,2,2}));
+        System.out.println(subsets2(new int[] { 1, 2, 2 }));
     }
+
     public static List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> list = new ArrayList<>();
         backtrack(list, new ArrayList<>(), nums, 0);
         return list;
     }
+
     private static void backtrack(List<List<Integer>> list, List<Integer> tempList, int[] nums, int start) {
         list.add(new ArrayList<>(tempList));
         for (int i = start; i < nums.length; i++) {
@@ -20,16 +22,19 @@ public class Subsets {
             tempList.remove(tempList.size() - 1);
         }
     }
+
     public static List<List<Integer>> subsets2(int[] nums) {
         List<List<Integer>> list = new ArrayList<>();
         Arrays.sort(nums);
         backtrack2(list, new ArrayList<>(), nums, 0);
         return list;
     }
+
     private static void backtrack2(List<List<Integer>> list, List<Integer> tempList, int[] nums, int start) {
         list.add(new ArrayList<>(tempList));
         for (int i = start; i < nums.length; i++) {
-            if(i > start && nums[i] == nums[i - 1]) continue;
+            if (i > start && nums[i] == nums[i - 1])
+                continue;
             tempList.add(nums[i]);
             backtrack(list, tempList, nums, i + 1);
             tempList.remove(tempList.size() - 1);
