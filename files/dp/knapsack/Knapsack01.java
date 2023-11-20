5d072a02f06c878f346a58fae2f5bae46b116b8b
chetan gupta <chetangupta123raj@gmail.com>
2023-11-20 15:05:02 +0530

    update dp

--- files/dp/knapsack/Knapsack01.java
+++ files/dp/knapsack/Knapsack01.java
@@ -4,7 +4,9 @@ import java.util.Arrays;
 
 public class Knapsack01 {
     public static void main(String[] args) {
-        System.out.println(knapsack(new int[] { 1, 2, 3 }, new int[] { 1, 3, 5 }, 4, 3));
+        // System.out.println(knapsack(new int[] { 1, 2, 3 }, new int[] { 1, 3, 5 }, 4,
+        // 3));
+        System.out.println(knapsackBottomUp(new int[] { 1, 2, 3 }, new int[] { 1, 3, 5 }, 4, 3));
     }
 
     public static int knapsack(int[] wt, int[] val, int cap, int n) {
@@ -26,6 +28,25 @@ public class Knapsack01 {
             return matrix[n][cap];
         }
         matrix[n][cap] = helper(wt, val, cap, n - 1, matrix);
+        return matrix[n][cap];
+    }
+
+    public static int knapsackBottomUp(int[] wt, int[] val, int cap, int n) {
+        int[][] matrix = new int[n + 1][cap + 1];
+        for (int i = 0; i < n + 1; i++) {
+            for (int j = 0; j < cap + 1; j++) {
+                if (i == 0 || j == 0)
+                    matrix[i][j] = 0;
+            }
+        }
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < cap + 1; j++) {
+                if (wt[i - 1] <= j) {
+                    matrix[i][j] = Math.max(val[i - 1] + matrix[i - 1][j - wt[i - 1]], matrix[i - 1][j]);
+                } else
+                    matrix[i][j] = matrix[i - 1][j];
+            }
+        }
         return matrix[n][cap];
     }
 }
---
+++ files/dp/knapsack/SubsetSum.java
@@ -0,0 +1,30 @@
+package dp.knapsack;
+
+public class SubsetSum {
+    public static void main(String[] args) {
+        System.out.println(subsetSum(new int[] { 2, 3, 7, 8, 10 }, 11));
+    }
+
+    public static boolean subsetSum(int[] arr, int sum) {
+        int n = arr.length;
+        boolean[][] matrix = new boolean[n + 1][sum + 1];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; i++) {
+                if (j == 0)
+                    matrix[i][j] = true;
+                else
+                    matrix[i][j] = false;
+            }
+        }
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < sum + 1; j++) {
+                if (arr[i - 1] <= j) {
+                    matrix[i][j] = matrix[i - 1][j - arr[i - 1]] || matrix[i - 1][j];
+                } else {
+                    matrix[i][j] = matrix[i - 1][j];
+                }
+            }
+        }
+        return matrix[n][sum];
+    }
+}
